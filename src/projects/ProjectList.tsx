7f7be4c69e1cfd9611f066cad2026a1df7dea26f
Angelica Bufano <abufano@tql.com>
2024-08-06 14:36:08 -0400

    update

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,21 +1,26 @@
-
-import { Project } from './Project';
-import ProjectCard from './ProjectCard';
-import ProjectForm from './ProjectForm';
-
+import React, { useState } from "react";
+import { Project } from "./Project";
+import ProjectCard from "./ProjectCard";
+import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
   projects: Project[];
 }
 
-function ProjectList ({ projects }: ProjectListProps) {
-  const items = projects.map(project => (
-    <div key={project.id} className="cols-sm">
-      <ProjectCard project={project}></ProjectCard>
-+       <ProjectForm />
+function ProjectList({ projects }: ProjectListProps) {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
+  const handleEdit = (project: Project) => {
+    setProjectBeingEdited(project);
+  };
+  return (
+    <div className="row">
+      {projects.map((project) => (
+        <div key={project.id} className="cols-sm">
+          {project === projectBeingEdited ? <ProjectForm /> : <ProjectCard project={project} onEdit={handleEdit} />}
+        </div>
+      ))}
     </div>
-  ));
-  return <div className="row">{items}</div>;
+  );
 }
 
 export default ProjectList;
